b97d4b0adf618b4e793fdd6a80814a121aad0a2f
sampson-q <quarmysokpoli@gmail.com>
2024-05-14 05:20:29 +0000

    master

---
+++ __main__.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from .config import Configuration
+except (ValueError, ImportError) as e:
+    raise Exception("You may need to run wifireconn from the root directory (which includes README.md)", e) from e
+
+
+from .util.color import Color
+
+import os
+import subprocess
+
+
+class Wifireconn(object):
+
+    def __init__(self):
+        """
+        Initializes Wifireconn.
+        Checks that its running under *nix, with root permissions and ensures dependencies are installed.
+        """
+
+        self.print_banner()
+
+        Configuration.initialize(load_interface=False)
+
+        if os.name == 'nt':
+            Color.pl('{!} {R}error: {O}wifireconn{R} must be run under a {O}*NIX{W}{R} like OS')
+            Configuration.exit_gracefully(0)
+        if os.getuid() != 0:
+            Color.pl('{!} {R}error: {O}wifireconn{R} must be run as {O}root{W}')
+            Color.pl('{!} {R}re-run with {O}sudo{W}')
+            Configuration.exit_gracefully(0)
+
+        from .tools.dependency import Dependency
+        Dependency.run_dependency_check()
+
+    def start(self):
+        """
+        Starts target-scan + attack loop, or launches utilities depending on user input.
+        """
+        from .model.result import CrackResult
+        from .model.handshake import Handshake
+        from .util.crack import CrackHelper
+
+        if Configuration.show_cracked:
+            CrackResult.display()
+
+        elif Configuration.check_handshake:
+            Handshake.check()
+
+        elif Configuration.crack_handshake:
+            CrackHelper.run()
+
+        else:
+            Configuration.get_monitor_mode_interface()
+            self.scan_and_attack()
+
+    @staticmethod
+    def print_banner():
+        """Displays ASCII art of the highest caliber."""
+        Color.pl(r' {G}  .     {GR}{D}     {W}{G}     .    {W}')
+        Color.pl(r' {G}.´  ·  .{GR}{D}     {W}{G}.  ·  `.  {G}wifireconn2 {D}%s{W}' % Configuration.version)
+        Color.pl(r' {G}:  :  : {GR}{D} (¯) {W}{G} :  :  :  {W}{D}a wireless auditor by derv82{W}')
+        Color.pl(r' {G}`.  ·  `{GR}{D} /¯\ {W}{G}´  ·  .´  {W}{D}maintained by kimocoder{W}')
+        Color.pl(r' {G}  `     {GR}{D}/¯¯¯\{W}{G}     ´    {C}{D}https://github.com/sampson-q/wifireconn{W}')
+        Color.pl('')
+
+    @staticmethod
+    def scan_and_attack():
+        """
+        1) Scans for targets, asks user to select targets
+        2) Attacks each target
+        """
+        from .util.scanner import Scanner
+        from .attack.all import AttackAll
+
+        Color.pl('')
+
+        # Scan
+        s = Scanner()
+        do_continue = s.find_targets()
+        targets = s.select_targets()
+
+        if Configuration.infinite_mode:
+            while do_continue:
+                AttackAll.attack_multiple(targets)
+                do_continue = s.update_targets()
+                if not do_continue:
+                    break
+                targets = s.select_targets()
+            attacked_targets = s.get_num_attacked()
+        else:
+            # Attack
+            attacked_targets = AttackAll.attack_multiple(targets)
+
+        Color.pl('{+} Finished attacking {C}%d{W} target(s), exiting' % attacked_targets)
+
+
+def entry_point():
+    try:
+        wifireconn = wifireconn()
+        wifireconn.start()
+    except Exception as e:
+        Color.pexception(e)
+        Color.pl('\n{!} {R}Exiting{W}\n')
+
+    except KeyboardInterrupt:
+        Color.pl('\n{!} {O}Interrupted, Shutting down...{W}')
+
+    # Delete Reaver .pcap
+    subprocess.run(["rm", "reaver_output.pcap"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    Configuration.exit_gracefully(0)
+
+
+if __name__ == '__main__':
+    entry_point()
---
+++ tools/dependency.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Dependency(object):
+    dependency_name = None
+    dependency_required = None
+    dependency_url = None
+    required_attr_names = ['dependency_name', 'dependency_url', 'dependency_required']
+
+    # https://stackoverflow.com/a/49024227
+    def __init_subclass__(cls):
+        for attr_name in cls.required_attr_names:
+            if attr_name not in cls.__dict__:
+                raise NotImplementedError(f'Attribute "{attr_name}" has not been overridden in class "{cls.__name__}"')
+
+    @classmethod
+    def exists(cls):
+        from ..util.process import Process
+        return Process.exists(cls.dependency_name)
+
+    @classmethod
+    def run_dependency_check(cls):
+        from ..util.color import Color
+
+        from .aircrack import Aircrack
+        from .ip import Ip
+        from .iw import Iw
+        from .bully import Bully
+        from .reaver import Reaver
+        from .tshark import Tshark
+        from .macchanger import Macchanger
+        from .hashcat import Hashcat, HcxDumpTool, HcxPcapngTool
+
+        apps = [
+            # Aircrack
+            Aircrack,  # Airodump, Airmon, Aireplay,
+            # wireless/net tools
+            Iw, Ip,
+            # WPS
+            Reaver, Bully,
+            # Cracking/handshakes
+            Tshark,
+            # Hashcat
+            Hashcat, HcxDumpTool, HcxPcapngTool,
+            # Misc
+            Macchanger
+        ]
+
+        missing_required = any(app.fails_dependency_check() for app in apps)
+
+        if missing_required:
+            Color.pl('{!} {O}At least 1 Required app is missing. Wifireconn needs Required apps to run{W}')
+            import sys
+            sys.exit(-1)
+
+    @classmethod
+    def fails_dependency_check(cls):
+        from ..util.color import Color
+        from ..util.process import Process
+
+        if Process.exists(cls.dependency_name):
+            return False
+
+        if cls.dependency_required:
+            Color.p('{!} {O}Error: Required app {R}%s{O} was not found' % cls.dependency_name)
+            Color.pl('. {W}install @ {C}%s{W}' % cls.dependency_url)
+            return True
+
+        else:
+            Color.p('{!} {O}Warning: Recommended app {R}%s{O} was not found' % cls.dependency_name)
+            Color.pl('. {W}install @ {C}%s{W}' % cls.dependency_url)
+            return False
