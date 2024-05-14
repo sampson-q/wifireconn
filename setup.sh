436d2212c4e54240377b56a566c436ac9629ee7f
sampson-q <quarmysokpoli@gmail.com>
2024-05-14 06:30:02 +0000

    wifireconn setup: (.sh file to setup wifireconn. intends to be used globally)

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Author: SAMPSON QUARMY, SOKPOLI (Hash 👽)
+
+# Add the current directory to the PATH environment variable
+export PATH=$PATH:$(pwd)
+
+# Make the main script executable
+chmod +x __main__.py
